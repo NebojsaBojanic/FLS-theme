80e25278641776986a5148a919dffd3418c0407a
MediavukProbaGit <bonitafcrs@yahoo.com>
2018-10-30 09:55:47 +0100

    New backup, responsive menu, mobile design

---
+++ backup/30102018/single-our_team.php
@@ -0,0 +1,101 @@
+<?php /* Template for custom posts Team Member */
+
+get_header(); ?>
+
+<?php 
+// Default settings
+	include("inc/theme-settings/theme-settings-defaults.php");
+	// Title
+	$titleColor		= get_field('team_title_color', 'option');
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+	<div class="nav-wrap grid-100">
+
+		<div class="nav team-single grid-1440 page-center clearfix">
+
+			<article class="nav-article grid-55 fl clearfix">
+
+				<h1 class="section-title <?php if ($titleColor) { echo $titleColor; } else { echo $defTitleColor; } ?>"><?php the_title(); ?></h1>
+
+				<?php
+					$fullName 			= get_field('full_name');
+					$image				= get_field('picture');
+					  $imgTitle 		= $image['title'];
+					  $imgAlt 			= $image['alt'];
+					$class 				= get_field('class');
+					$description 		= get_field('description');
+					// Social media
+					$facebook 			= get_field('facebook');
+					$twitter 			= get_field('twitter');
+					$googlePlus 		= get_field('google_plus');
+					$instagram 			= get_field('instagram');
+					$linkedin 			= get_field('linkedin');
+				?>
+
+					<figure>
+						<img src="<?php echo $image['sizes']['img_300x320']; ?>" alt="<?php echo $imgAlt; ?>" title="<?php echo $imgTitle; ?>">
+					</figure>
+
+					<div class="team-member-info">
+
+						<h3><?php echo $fullName; ?></h3>
+						<span><?php echo $class; ?></span>
+						<?php echo $description; ?>
+						<ul class="social-media">
+							<?php // Facebook
+							if( $facebook ):
+								if ($facebook['display_facebook']) { ?>
+									<li><a href="<?php echo $facebook['facebook_url']; ?>" target="_blank">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/Facebook_icon_color.png">
+									</a></li>
+								<?php }
+							endif; ?>
+							<?php // Twitter
+							if( $twitter ):
+								if ($twitter['display_twitter']) { ?>
+									<li><a href="<?php echo $twitter['twitter_url']; ?>" target="_blank">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/Twitter_icon_color.png">
+									</a></li>
+								<?php }
+							endif; ?>
+							<?php // Google Plus
+							if( $googlePlus ):
+								if ($googlePlus['display_google_plus']) { ?>
+									<li><a href="<?php echo $googlePlus['google_plus_url']; ?>" target="_blank">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/Google-plus_icon_color.png">
+									</a></li>
+								<?php }
+							endif; ?>
+							<?php // Instagram
+							if( $instagram ):
+								if ($instagram['display_instagram']) { ?>
+									<li><a href="<?php echo $instagram['instagram_url']; ?>" target="_blank">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/Instagram_icon_color.png">
+									</a></li>
+								<?php }
+							endif; ?>
+							<?php // LinkedIn
+							if( $linkedin ):
+								if ($linkedin['display_linkedin']) { ?>
+									<li><a href="<?php echo $linkedin['linkedin_url']; ?>" target="_blank">
+										<img src="<?php echo get_template_directory_uri(); ?>/images/Linkedin_icon_color.png">
+									</a></li>
+								<?php }
+							endif; ?>
+						</ul>
+
+					</div>
+
+			</article>
+
+			<?php get_sidebar('our_team'); ?>
+
+		</div><!-- END: nav -->
+
+	</div><!-- END: nav-wrap -->
+
+	<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
---
+++ backup/30102018/tag.php
@@ -0,0 +1,103 @@
+<?php
+/**
+* The template shows Tags Page
+*
+*/
+get_header(); ?>
+
+<?php
+// Default values
+	include("inc/theme-settings/theme-settings-defaults.php");
+	// Title
+	$titleColor			= get_field('blog_heading_color', 'option');
+	// Buttons
+	$buttonText 		= get_field('blog_text_on_button', 'option');
+	$buttonTextColor 	= get_field('blog_text_color_on_button', 'option');
+	$buttonBg 			= get_field('blog_button_color', 'option');
+?>
+
+<div class="blog-wrap grid-100">
+
+	<div class="tag-title">
+		<?php single_tag_title( '<h1 class="section-title">Tag: ', '</h1>' ); ?>
+	</div>
+
+	<div class="blog grid-1440 clearfix">
+
+		<section class="aktuelles grid-50 fl clearfix">
+
+			<div class="tags-results">
+
+				<?php if (have_posts()) :
+
+					while (have_posts()) : the_post();
+						$image		= get_field('image');
+							$url 	= $image['url'];
+							$title 	= $image['title'];
+							$alt 	= $image['alt'];
+						$date 		= get_the_date('l, F j, Y');
+
+						// Full name of author
+						$fname 		= get_the_author_meta('first_name');
+						$lname 		= get_the_author_meta('last_name');
+						$full_name 	= '';
+
+						if ( empty($fname) ) {
+							$full_name = $lname;
+						} elseif( empty( $lname )) {
+							$full_name = $fname;
+						} else {
+							//both first name and last name are present
+							$full_name = "{$fname} {$lname}";
+						}
+				?>
+
+					<article class="recent-post single-article">
+						<a href="<?php the_permalink(); ?>" title="Read more about <?php the_title(); ?>">
+							<header class="article-header">
+								<h3 class="text-grey"><?php the_title(); ?></h3>
+								<span class="article-info"><?php echo $date; ?><b><span class="pipe text-red"> | </span><i><?php echo $full_name; ?></i></b></span>
+							</header>
+							<?php if ($image): ?>
+								<figure class="grid-100">
+									<img src="<?php echo $image['sizes']['img_four_three']; ?>" alt="<?php echo $alt; ?>" title="<?php echo $title; ?>">
+								</figure>
+							<?php endif ?>
+							<p><?php the_field("excerpt"); ?></p>
+						</a>
+
+						<div class="permalink">
+							<a href="<?php the_permalink(); ?>" title="Read more about <?php the_title(); ?>" class="fls-button <?php if ($buttonBg) { echo $buttonBg; } else { echo $defButtonBg; } ?> <?php if ($buttonTextColor) { echo $buttonTextColor; } else { echo $defButtonTextColor; } ?>">
+								<?php if ($buttonText) { echo $buttonText; } else { echo $defButtonText; } ?>
+							</a>
+						</div><!-- end: permalink -->
+
+					</article><!-- END: Aktuelles article -->
+
+					<?php endwhile;
+
+				endif; ?>
+
+			</div><!-- END: tags-results -->
+
+			<div class="clearfix"></div>
+
+			<div class="pagination textCenter grid-100 <?php if ($titleColor) { echo $titleColor; } else { echo $defTitleColor; } ?>">
+
+				<?php the_posts_pagination( array(
+					'mid_size'  => 2,
+					'prev_text' => "<",
+					'next_text' => ">"
+				) ); ?>
+
+			</div><!-- END: pagination-->
+
+		</section>
+
+		<?php get_sidebar(); ?>
+
+	</div><!-- END: blog -->
+
+</div><!-- END: blog-wrap -->
+	
+<?php get_footer(); ?>
